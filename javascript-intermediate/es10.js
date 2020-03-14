80429fc7ac2b865ebfa53055b08fd0ec89b945b7
codefuldom <dom@codefuldom.com>
2020-03-14 15:10:47 -0500

    :rocket: flatten that bullshit nested array

--- javascript-intermediate/es10.js
+++ javascript-intermediate/es10.js
@@ -1,2 +1,5 @@
-const flatten = [1, [2, 3], [4, 5]]
-console.log(flatten.flat())
+const flatten = [1, [2, 3], [4, [5]]]
+console.log(flatten.flat(2))
+
+const jurassicPark = [['🏇🏾', '🐕'], [['😺']], [[['🐸']]]]
+console.log(jurassicPark.flat(3))
