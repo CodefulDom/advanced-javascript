9e42b97fa42eaaee35fb3bcb27f47ff4faedf742
codefuldom <dom@codefuldom.com>
2020-03-14 15:17:18 -0500

    :rocket: use the flatMap and trimStart methods

--- javascript-intermediate/es10.js
+++ javascript-intermediate/es10.js
@@ -1,5 +1,12 @@
 const flatten = [1, [2, 3], [4, [5]]]
 console.log(flatten.flat(2))
 
-const jurassicPark = [['🏇🏾', '🐕'], [['😺']], [[['🐸']]]]
+const jurassicPark = ['🏇🏾', '🐕', [['😺']], ['🐸']]
 console.log(jurassicPark.flat(3))
+const entries = ['bob', 'sally', , , , , , , , 'cindy']
+console.log(entries.flat())
+const jurassicParkChaos = jurassicPark.flatMap((creature) => `${creature} 💩`)
+console.log(jurassicParkChaos)
+
+const userEmail = '         eddytheEagle@gmail.com'
+console.log(userEmail.trimStart())
