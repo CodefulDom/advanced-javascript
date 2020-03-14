ff2c24a7fd8062f110462e0d13e29234b52f06de
codefuldom <dom@codefuldom.com>
2020-03-14 12:22:34 -0500

    :rocket: add symbol to es6.js

--- javascript-intermediate/es6.js
+++ javascript-intermediate/es6.js
@@ -25,3 +25,23 @@ const newObj = {
 }
 
 console.log(newObj.alpha)
+
+function greeting(name = 'Dom', age = 37, pet = '🐕') {
+  return `Hello, ${name} you are ${age}. What a lovely ${pet}.`
+}
+
+greeting() /* ? */
+
+let sym1 = Symbol('foo')
+let sym2 = Symbol('foo foo')
+let sym3 = Symbol('foo foo foo')
+
+console.log(sym1)
+console.log(sym2)
+console.log(sym3)
+
+const symbolObject = {
+  one: Symbol('one')
+}
+
+console.log(symbolObject.one)
