40aecd0c7754aa7b3a3cfc48a0bf420e8e514de4
codefuldom <dom@codefuldom.com>
2020-03-14 13:09:37 -0500

    :rocket: add composed function to advancedFunction

--- javascript-intermediate/advancedFunctions.js
+++ javascript-intermediate/advancedFunctions.js
@@ -12,4 +12,10 @@ newFunc()
 // Currying
 const multiply = (a, b) => a * b
 const curriedMultiply = (a) => (b) => a * b
-curriedMultiply(3)(6) /* ? */
+const multiplyBy5 = curriedMultiply(5)
+console.log(multiplyBy5(5))
+
+// compose: take 2 functions and combine them so the output of one function is returned in the other function
+const compose = (f, g) => (a) => f(g(a))
+const sum = (num) => num + 1
+compose(sum, sum)(5) /* ? */
