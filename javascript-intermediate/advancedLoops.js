93446e395286e1fea9291f7e7da88f39a9091579
codefuldom <dom@codefuldom.com>
2020-03-14 15:35:26 -0500

    :rocket: learn the difference between enumeration and iteration

---
+++ javascript-intermediate/advancedLoops.js
@@ -0,0 +1,16 @@
+const groceries = ['apples', 'grapes', 'oranges']
+
+for (let i = 0; i < groceries.length; i++) {
+  const element = groceries[i]
+  console.log(element)
+}
+
+const detailedObject = {
+  oranges: 10,
+  apples: 20,
+  grapes: 1000
+}
+
+for (const properties in detailedObject) {
+  console.log(properties)
+}
